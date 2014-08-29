af1c6fb625520c383fe5c7334165c774052ec52f
erik-smit <smite@zylon.net>
2014-08-29 16:40:51 +0200

    faster harder stronger je weet wel

---
+++ scripts/target-bully.sh
@@ -0,0 +1,31 @@
+#
+# this script dangles the target ECU by the legs 
+# and shakes until all lunch money is on the floor
+#
+
+#!/bin/bash
+( echo ATZ; sleep 1;
+echo ATE0; sleep 1;
+echo ATL1; sleep 1;
+echo ATH0; sleep 1;
+echo ATSP5; sleep 1;
+echo ATSH8111F1; sleep 1;
+echo ATFI; sleep 1;
+echo 18 00 FF 00; sleep 1;
+for i in `seq 0 255`; do 
+  echo 1A `printf %02X $i`;
+  sleep 1.0
+done ;
+for i in `seq 0 255`; do 
+  echo 21 `printf %02X $i`;
+  sleep 1.0
+done ;
+for i in `seq 0 255`; do 
+  echo 30 `printf %02X $i` 01;
+  sleep 1.0
+done ;
+for i in `seq 0 255`; do 
+  echo 33 `printf %02X $i`;
+  sleep 1.0
+done ) | 
+  socat - tcp-connect:192.168.0.10:35000,cr
